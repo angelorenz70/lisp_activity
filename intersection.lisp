606691c6c3402fa2efe494c40990919b867b35bc
unknown <angelorenz70@gmail.com>
2023-11-16 22:34:47 +0800

    complement and intersection

---
+++ intersection.lisp
@@ -0,0 +1,10 @@
+(defun custom-intersection (list1 list2)
+  (mapcar (lambda (x) (when (member x list1 :test #'equal) x)) list2))
+
+
+(setq set-A '(1 2 (3 10) 4 5))
+(setq set-B '(3 4 5 6 7 (3 10) 10))
+
+(setq result (custom-intersection set-A set-B))
+(setq result (remove-if #'null result))
+(format t "Element that belong to A and also belong B are : ~%~a" result)
