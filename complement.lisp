bb26498f633eb5a23ae274d2e8a5c4f76d734610
metseDevs <angelorenz70@gmail.com>
2025-01-06 19:16:08 +0800

    activbites

--- complement.lisp
+++ complement.lisp
@@ -1,9 +1,9 @@
-(defun custom-complement (list1 list2)
+(defun complement-set (list1 list2)
   (mapcar (lambda (x) (when (not(member x list2 :test #'equal)) x)) list1))
 
 (setq set-A '(1 2 (3 10) 4 5))
 (setq set-B '(3 4 5 6 7 (3 10) 10))
 
-(setq result (custom-complement set-A set-B))
+(setq result (complement-set set-A set-B))
 (setq result (remove-if #'null result))
 (format t "Elements that belong to A but not belong to B are : ~%~a" result)
